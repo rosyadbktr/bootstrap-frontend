5c803a50b805bb05e6a020b797d8f24ead55e800
supeRosyad <rosyadandro@gmail.com>
2023-05-08 21:52:11 +0700

    menghapus script di index.html dan menghapus file script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-let sosmedLink = document.getElementsByClassName("medsos-link");
-// console.log(sosmedLink[0]);
-let sosmedLinkChanges = (x) => x.classList.replace("text-secondary", "text-primary");
-sosmedLink[0].addEventListener("mouseover", sosmedLinkChanges);
